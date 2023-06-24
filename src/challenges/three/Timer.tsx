42ffcf36b106009e6949c08bad06f159404bcee8
sean <sean.rojas@flexisourceit.com.au>
2023-06-24 14:31:37 +0800

    utilize timer context data/event handlers

--- src/challenges/three/Timer.tsx
+++ src/challenges/three/Timer.tsx
@@ -1,15 +1,44 @@
 import { Button, HStack, Text } from "@chakra-ui/react";
-import React from "react";
+import { useEffect } from "react";
+
+import TimerContextProvider, { useTimer } from './TimerContext'
 
 type TimerProps = {
   children: JSX.Element;
 };
 
+type TimerResult = {
+  minutes: string
+  seconds: string
+  milliseconds: string
+}
+
+
+const getDisplayTime = (currentTime: number): TimerResult => {
+  const milliseconds = (currentTime % 100).toString().padStart(2, "0")[1];
+  const secondsCompute = (Math.floor(currentTime / 10)) % 60;
+  const seconds = secondsCompute >= 1 ? secondsCompute.toString().padStart(2, "0") : "00"
+  const minutes = (currentTime > 600 ? Math.floor(currentTime / 600) : 0).toString().padStart(2, "0");
+
+  return {
+    minutes,
+    seconds,
+    milliseconds
+  }
+}
+
 export default function Timer({ children }: TimerProps) {
-  return children;
+  return (
+    <TimerContextProvider>
+      {children}
+    </TimerContextProvider>
+  );
 }
 
 export function TimerDisplay() {
+  const { timeCount: { value } } = useTimer()
+  const { minutes, seconds, milliseconds } = getDisplayTime(value)
+
   return (
     <Text
       fontSize="7xl"
@@ -18,17 +47,24 @@ export function TimerDisplay() {
         fontVariantNumeric: "tabular-nums"
       }}
     >
-      0:00:0
+      {minutes}:{seconds}:{milliseconds}
     </Text>
   );
 }
 
 export function TimerControls() {
+  const { timeCount: { action, value }, timerAction: { START, PAUSE, RESET } } = useTimer()
   return (
     <HStack>
-      <Button colorScheme="green">Start</Button>
-      <Button colorScheme="red">Pause</Button>
-      <Button>Reset</Button>
+      <Button
+        {...action === 'PAUSE' && ({ display: 'none' })}
+        onClick={START} colorScheme="green">Start</Button>
+      <Button
+        {...action === 'START' && ({ display: 'none' })}
+        onClick={PAUSE} colorScheme="red">Pause</Button>
+      <Button
+        {...value <= 0 && ({ display: 'none' })}
+        onClick={RESET} >Reset</Button>
     </HStack>
   );
 }
